06654b6d57ea3617c5349e47cedad6a564a72383
Dmytro <dima17lit@gmail.com>
2020-03-22 15:31:14 +0200

    release v1

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("My first debug")
+
+let age = 23
+let NameOfMyDog = "Jack"
+console.log(age)
+console.log(NameOfMyDog)
+
+age = 45
+NameOfMyDog = "George"
+console.log(age)
+console.log(NameOfMyDog)
+
+let number = 10
+let string = "Dima"
+let boolean = true
+let array = [10,2,3,false,"Don"]
+let object = {
+    name:"Jack",
+    age:10}
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof array)
+console.log(typeof object)
+
+const student = {
+    name:"Dima",
+    surname:"Basamyhin",
+    isActive:true}
+console.log(typeof student.isActive)
+console.log(`Student: ${student.name} ${student.surname}, now is active: ${student.isActive}`)
+
+const newArray = [{fruitfirst:"apple", fruitsecond:"pineapple"} , {numberfirst:10, numbersecond:20} , {nameOfStudent:"dima",surnameOfStudent:"basamyhin"} , {nameOfSecondStudent:"Nazar",surnameOfSecondStudent:"Nazarchuk"} , {nameOfThirdName:"Alex" , nameOfThirdStudent:"Adamov"} ]
+console.log(newArray)
+
+let star = [0,1,2,3,4,5,6,7,8,9]
+console.log(star[0],star[9])
+
